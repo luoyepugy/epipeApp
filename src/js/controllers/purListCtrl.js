7b561ed1fcf9a16a2a36eb3b48dff3cfef302578
zhanghan <1119432095@qq.com>
2015-12-10 15:09:29 +0800

    样式图标修改完成

--- src/js/controllers/purListCtrl.js
+++ src/js/controllers/purListCtrl.js
@@ -52,39 +52,5 @@ define(['./module'], function(controllers) {
 	            $scope.$broadcast('scroll.infiniteScrollComplete');
 		    });
 	    };
-	    // 商家报价&&物流追踪
-	    $scope.listDetail = function() {
-	    	var status = this.item.status;
-	    	var id = this.item.id;
-	    	if(status === null) {
-				var offerNum = this.item.offerNum;
-				if(offerNum > 0) {
-					// 预加载
-				    $ionicLoading.show({
-				        template: '<ion-spinner></ion-spinner><h3>加载中...</h3>',
-				        duration: 3000
-				    });
-					httpService.getData('./json/login.json', {'id': id})
-				    .then(function(data) {
-				    	$ionicLoading.hide();
-				    	$state.go('purchase.offer');
-				    });
-				}　else {
-					messageService.show('暂时没有商家报价');
-				}
-	    	}　else {
-				// 预加载
-			    $ionicLoading.show({
-			        template: '<ion-spinner></ion-spinner><h3>加载中...</h3>',
-			        duration: 3000
-			    });
-				httpService.getData('./json/login.json', {'id': id})
-			    .then(function(data) {
-			    	$ionicLoading.hide();
-			    	$state.go('purchase.logistics');
-			    });
-	    	}
-	    };
-
 	}]);
 });
--- src/js/directives/offerDctv.js
+++ src/js/directives/offerDctv.js
@@ -1,18 +1,33 @@
 
 define(['./module'], function(directives) {
-	directives.directive('offerDetail', ['httpService', '$ionicLoading', 'messageService',
-		function(httpService, $ionicLoading ,messageService) {
+	directives.directive('listDetail', ['httpService', '$ionicLoading', 'messageService', '$state',
+		function(httpService, $ionicLoading ,messageService, $state) {
 		return {
-			restrict: 'A',
+			restrict: 'AE',
 			template: '',
 			replace: false,
 			scope: false,
 			link: function(scope, element, attrs) {
 				element.on('click', function() {
-					console.log('dfs');
-					var id = this.$parent.item.id;
-					var status = this.$parent.item.status;
-					if(status > 0) {
+					var status = scope.$parent.item.status;
+			    	var id = scope.$parent.item.id;
+			    	if(status === null) {
+						var offerNum = scope.$parent.item.offerNum;
+						if(offerNum > 0) {
+							// 预加载
+						    $ionicLoading.show({
+						        template: '<ion-spinner></ion-spinner><h3>加载中...</h3>',
+						        duration: 3000
+						    });
+							httpService.getData('./json/login.json', {'id': id})
+						    .then(function(data) {
+						    	$ionicLoading.hide();
+						    	$state.go('purchase.offer');
+						    });
+						}　else {
+							messageService.show('暂时没有商家报价');
+						}
+			    	}　else {
 						// 预加载
 					    $ionicLoading.show({
 					        template: '<ion-spinner></ion-spinner><h3>加载中...</h3>',
@@ -21,11 +36,9 @@ define(['./module'], function(directives) {
 						httpService.getData('./json/login.json', {'id': id})
 					    .then(function(data) {
 					    	$ionicLoading.hide();
-					    	$state.go('purchase.offer');
+					    	$state.go('purchase.logistics');
 					    });
-					}　else {
-						messageService.show('暂时没有商家报价');
-					}
+			    	}
 				});
 			}
 		};
