a94258c6a788a9ab12319fb49ebbaed48581d8b9
zhanghan <1119432095@qq.com>
2015-12-23 09:51:55 +0800

    修改下拉列表指令js

--- src/js/directives/dropdownListDctv.js
+++ src/js/directives/dropdownListDctv.js
@@ -4,7 +4,7 @@ define(['./module'], function(directives) {
 		return {
 			restrict: 'AE',
             template: '<div>' +
-                        '<input type="text" class="{{inputClass}}"　 name="{{name}}" placeholder="{{placeholder}}" ng-model="val" value="{{val}}"  data-empty="{{empty}}" />' +
+                        '<input type="text" ng-blur="blur()" class="{{inputClass}}"　 name="{{name}}" placeholder="{{placeholder}}" ng-model="val" value="{{val}}"  data-empty="{{empty}}" />' +
                         '<ion-scroll class="downList" ng-if="downListShow">' +
                             '<ul>' +
                                 '<li ng-click="choiceItem()" ng-repeat="item in list">{{item.name}}</li>' +
@@ -33,6 +33,9 @@ define(['./module'], function(directives) {
                         });
                     }
                 });
+                scope.blur = function() {
+                    scope.downListShow　= false;
+                };
                 scope.choiceItem = function() {
                     scope.val = this.item.name;
                     scope.downListShow　= false;
