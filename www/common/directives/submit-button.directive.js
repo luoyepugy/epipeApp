26b008b440e361a629386c4cc959ab76e81d2d2c
zhanghan <1119432095@qq.com>
2016-03-14 16:58:23 +0800

    修改发布页面,地点字段为必填

--- www/common/directives/submit-button.directive.js
+++ www/common/directives/submit-button.directive.js
@@ -18,6 +18,7 @@ define(['../common.module'], function(common) {
         function link(scope, element, attrs) {
             scope.text = attrs.text || '提交保存';
             var state = attrs.state || '',
+                stateParams = attrs.stateParams || '',
                 user = attrs.user || '',
                 login = attrs.login || '',
                 phone_regexp = /^((145|147)|(15[^4])|(17[6-8])|((13|18)[0-9]))\d{8}$/,
@@ -41,7 +42,13 @@ define(['../common.module'], function(common) {
                     httpService.getDatas(method, actionpath + attrs.action, resultsDatas)
                     // httpService.get(method, action, resultsDatas);
                     .then(function(data) {
-                        $state.go(state);
+                        
+                        if(stateParams = '所有') {
+                            $state.go(state, {'state': '所有'});
+                        } else {
+                            $state.go(state);
+                        }
+                        
                         if(login === 'true') {
                             $window.localStorage.token = data.token;
                         }
